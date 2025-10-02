e860e2832fd9796dc04bc2d0748062ffa7bd1e99
Morgan <morganthewit@gmail.com>
2025-10-02 14:04:57 +0100

    Rock 1
    - Began work on spawning rock obstacles into the game

--- Assets/Scripts/CoreScript.cs
+++ Assets/Scripts/CoreScript.cs
@@ -4,13 +4,32 @@ public class CoreScript : MonoBehaviour
 {
     public float scrollSpeed;
 
+    //Rock Obstacle Refs
+    public float rockSpawnRate;
+    private float rockSpawnTime;
+
     void Awake()
     {
         Screen.SetResolution(400, 800, false);
     }
 
+    void Update()
+    {
+        RockSpawning();
+    }
+
     public void EndlessScroll(Rigidbody2D rigidbody)
     {
         rigidbody.linearVelocityY = 0.0f - scrollSpeed;
+    }
+
+    void RockSpawning()
+    {
+        rockSpawnTime = Time.deltaTime;
+        if (rockSpawnTime == rockSpawnRate)
+        {
+            rockSpawnTime -= rockSpawnRate;
+
+        }
     }
 }
---
+++ Assets/Scripts/RockScript.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class RockScript : MonoBehaviour
+{
+    private CoreScript core;
+    
+    void Start()
+    {
+        
+    }
+
+}
