88fd981b3b58c09cd43fc45b92eb06f0746ec313
DragonPollyanna <morganthewit@gmail.com>
2025-10-02 21:21:37 +0100

    Rock 2
    - Added rock objects that spawn at the top of the level with a random position, rotation and scale. Travel down the screen at the same pace as the player.

--- Assets/Scripts/CoreScript.cs
+++ Assets/Scripts/CoreScript.cs
@@ -5,7 +5,10 @@ public class CoreScript : MonoBehaviour
     public float scrollSpeed;
 
     //Rock Obstacle Refs
+    public GameObject rock;
     public float rockSpawnRate;
+    public float rockMinimumSize;
+    public float rockMaximumSize;
     private float rockSpawnTime;
 
     void Awake()
@@ -25,11 +28,11 @@ public class CoreScript : MonoBehaviour
 
     void RockSpawning()
     {
-        rockSpawnTime = Time.deltaTime;
-        if (rockSpawnTime == rockSpawnRate)
+        rockSpawnTime += Time.deltaTime;
+        if (rockSpawnTime >= rockSpawnRate)
         {
             rockSpawnTime -= rockSpawnRate;
-
+            Instantiate(rock, new Vector3(Random.Range(-2.0f, 2.0f), 6.0f, 0.0f), Quaternion.identity);
         }
     }
 }
--- Assets/Scripts/RockScript.cs
+++ Assets/Scripts/RockScript.cs
@@ -3,10 +3,30 @@ using UnityEngine;
 public class RockScript : MonoBehaviour
 {
     private CoreScript core;
-    
+    public Rigidbody2D rock;
+
+    void Awake()
+    {
+        core = Object.FindAnyObjectByType<CoreScript>();
+    }
+
     void Start()
     {
-        
+        transform.localScale = new Vector3(Random.Range(core.rockMinimumSize, core.rockMaximumSize), Random.Range(core.rockMinimumSize, core.rockMaximumSize), 0.0f);
+        transform.Rotate(0.0f, 0.0f, Random.Range(0.0f, 360.0f));
+    }
+
+    void Update()
+    {
+        core.EndlessScroll(rock);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("DestroyZone"))
+        {
+            Destroy(gameObject);
+        }
     }
 
 }
